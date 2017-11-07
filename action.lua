f2087881a6640e1f3e02bc62abe97c0acc78613a
Bas Pennings <baspster@gmail.com>
2017-11-07 14:45:55 +0100

    Fixes to action module

--- action.lua
+++ action.lua
@@ -2,6 +2,12 @@ action = action or {}
 
 action.actions = action.actions or {}
 
+function action:list()
+    for pat, act in pairs(self.actions) do
+        cecho(string.format("%-24s %s\n", pat, act.code))
+    end
+end
+
 function action:create(pattern, code)
     self.actions[pattern] = {
         code = code,
